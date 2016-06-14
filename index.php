aa005c4f308579ab1de354350f3d1740a57d7d3c
luisbch9 <luisbch9@gmail.com>
2016-06-14 11:57:41 -0500

    Hello World
    Test Apache

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tramite Documentario</title>
+</head>
+<body>
+<h1>	Hello World¡</h1>
+
+<?php 	
+	
+	echo "<h2>Si ves esto ... funciona tu PHP</h2>";
+
+ ?>
+</body>
+</html>
