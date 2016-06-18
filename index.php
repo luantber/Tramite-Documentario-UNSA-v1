b4a9e0ecfd00f02c00ace4a83eed3232433a0ed9
luisbch9 <luisbch9@gmail.com>
2016-06-17 20:35:25 -0500

    ?

--- index.php
+++ index.php
@@ -9,7 +9,11 @@
 <?php 	
 	
 	echo "<h2>Si ves esto ... funciona tu PHP</h2>";
-
+	echo "Comiteo"
+	echo "<h3> Esto esta en <b>html</b> </h3>"
+	for ($i=0; $i < 10 ; $i++) { 
+		echo $i;
+	}
  ?>
 </body>
 </html>
