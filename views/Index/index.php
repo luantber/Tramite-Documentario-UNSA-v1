9215a2c2ef6f21bbb066cff207564664259805da
margarcuae <margar.tcuae@gmail.com>
2016-06-18 23:13:49 -0500

    m1

--- views/Index/index.php
+++ views/Index/index.php
@@ -1,14 +1,20 @@
 
 <div id="Inicio" align="center">
-<div id="Cliente" class="azul-claro">
-	<h1 class="azul-oscuro" align="center">Cliente Nuevo</h1>
-	<form action="" method="post">
-		<fieldset>
-			<label for="name">Nombre:</label>
-			<input type="text" id="name" placeholder="Ingrese nombre cliente"/>
 
 	<h1 class="grande azul-oscuro">BIENVENIDOS AL SI</h1>
 	<h3 class="pequeno carne">sitema inificiente</h3>	
 	<h2 class="azul-claro ">TRÁMITE DOCUMENTARIO</h2>
+
+	<form action="#" method="post">
+		<fieldset>
+
+			<input required type="text" id="juser" placeholder="Ingrese nombre de usuario" >
+			<input required type="password" id="juser" placeholder="Ingrese contraseña" >		
+
+			<button type="submit">Iniciar sesión</button>
+
+		</fieldset>
+		
+	</form>
 
 </div>
