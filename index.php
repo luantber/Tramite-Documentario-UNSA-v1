f65c2c35f11b7e9c400cc0e1003a7d5257fd5dee
luisbch9 <luisbch9@gmail.com>
2016-06-20 04:38:05 -0500

    define URL Views .. con mayuscula

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 	//Deben ser rutas absolutas porque el router las va a transformar
 	require_once 'config.php';
-	define('URL', 'http://localhost/'.$nombre_carpeta.'/views/');
+	define('URL', 'http://localhost/'.$nombre_carpeta.'/Views/');
 	define('URLM', 'http://localhost/'.$nombre_carpeta.'/');
 
 	//Carga automatica de clases
