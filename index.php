a0efc5e5a237b7ef6169ed17f76b5b9576a31074
lamlemonpie <alejandrolarraondo@gmail.com>
2016-06-20 13:33:39 -0500

    Carpetas

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tramite Documentario</title>
-</head>
-<body>
-<h1>	Hello World! </h1>
 
-	<?php
-		include "Include.php";
-		echo "<h2>Si ves esto ... funciona tu PHP</h2>";
-		echo "<h2> Sistema SI </h2>";
-		$prub = new Empleado;
-		$prub -> _Empleado();
-	?>
-</body>
-</html>
-=======
-<?php 	
+<?php
 
 
 	define('DS', DIRECTORY_SEPARATOR);
@@ -35,4 +17,3 @@
 	Config\Router::run(new Config\Request());
 
 ?>
->>>>>>> origin/master
