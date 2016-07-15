a4f9dc8184318ee829c395a883ddccf1c610d182
texsmv <texs.mv@gmail.com>
2016-07-15 11:14:53 -0500

    Update Include.php

--- Models/Include.php
+++ Models/Include.php
@@ -3,10 +3,5 @@
   define("username","root",TRUE);
   define("password","",TRUE);
   define("dbname","database4",TRUE);
-  define("tabla_personas","personas",TRUE);
-  include_once "Query.php";
-  include_once "Empleado.php";
-  include_once "Registrador.php";
-  include_once "Persona.php";
-
+  
  ?>
