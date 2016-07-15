840ee1921585405cbd02518d5849a55260f8b03e
texsmv <texs.mv@gmail.com>
2016-07-15 10:14:03 -0500

    Añadidas funciones para modificar datos

--- Models/Empleado.php
+++ Models/Empleado.php
@@ -2,111 +2,113 @@
 		/**
 		* 	
 		*/
-		
 		use Models\Query as Query;
 		include_once "Include.php";
-		class Empleado extends Persona
+		class Persona
 		{
-			
-			var $id_cargo;
-			var $id_area;
-			var $activo;
-			var $correo;
-			var $password;
+			var $query;
+			var $id;
+			var $dni;
+			var $nombres;
+			var $apellidos;
+			var $nombre_empresa;
 
-			public function  __construct($Id_Empleado){
+
+			function  __construct($Id_Persona){
+
 				$this->query =  new Query();
-				$request_empleado="SELECT `Id_Empleado`, `Id_Cargo`, `Id_Area`, `Activo`, `Correo`, `Dni_Empleado`, `Password` FROM `empleados` WHERE Id_Empleado=".$Id_Empleado;
-				$result_empleado=$this->query->consulta($request_empleado);
-				if($result_empleado->num_rows != 0){
-					$datos_empleado = $result_empleado->fetch_assoc();
-					$this->id=$datos_empleado["Id_Empleado"];
-					$this->id_cargo=$datos_empleado["Id_Cargo"];
-					$this->id_area=$datos_empleado["Id_Area"];
-					$this->activo=$datos_empleado["Activo"];
-					$this->correo=$datos_empleado["Correo"];
-					$this->id_cargo=$datos_empleado["Id_Cargo"];
-					$this->dni=$datos_empleado["Dni_Empleado"];
-					$this->password=$datos_empleado["Password"];
-				
-					$request_persona="SELECT `Id_Persona`, `Dni`, `Nombres`, `Apellidos`, `Nombre_Empresa` FROM `personas` WHERE Id_Persona=".$Id_Empleado;
-					$result2=$this->query->consulta($request_persona);
-				    $datos_persona = $result2->fetch_assoc();				    			
-				    $this->nombres=$datos_persona["Nombres"];
-				    $this->apellidos=$datos_persona["Apellidos"];
-				    $this->nombre_empresa=$datos_persona["Nombre_Empresa"];
-					return true;
-				}
-				else{
-					return false;
+				$request="SELECT `Id_Persona`, `Dni`, `Nombres`, `Apellidos`, `Nombre_Empresa` FROM `personas` WHERE Id_Persona=".$Id_Persona;
+				$result=$this->query->consulta($request);
+				if ($result->num_rows != 0) {
+				    $datos = $result->fetch_assoc();				    			    
+				    $this->id=$datos["Id_Persona"];
+				    $this->dni=$datos["Dni"];
+				    $this->nombres=$datos["Nombres"];
+				    $this->apellidos=$datos["Apellidos"];
+				    $this->nombre_empresa=$datos["Nombre_Empresa"];
+				    return true;				    
+				} 
+				else {
+				    return false;
 				}
 
-					
+			}
 
+			public function get_id()
+			{
+				return $this->id;
 			}
 			
-			public function get_nombre_cargo()
+			public function get_nombres()
 			{
-				$request="SELECT Nombre_cargo FROM empleados  JOIN cargos ON empleados.Id_Cargo=cargos.Id_Cargo WHERE Id_Empleado=".$this->id;
-				$result=$this->query->consulta($request);
-				$datos=$result->fetch_assoc();
-
-				return $datos["Nombre_cargo"];
+				return $this->nombres;
 			}
 
-			public function get_id_cargo()
+			public function cambiar_nombres($Nombres)
 			{
-				return $this->id_cargo;
+				$request="UPDATE `personas` SET `Nombres`='".$Nombres."' WHERE Id_Persona=".$this->id;
+				$this->query->consulta($request);
+				$this->nombres=$Nombres;
 			}
 
-			public function get_id_area()
+			public function get_apellidos()
 			{
-				return $this->id_area;
+				return $this->apellidos;
 			}
 
-			public function get_nombre_area()
+			public function cambiar_apellidos($Apellidos)
 			{
+				$request="UPDATE `personas` SET `Apellidos`='".$Apellidos."' WHERE Id_Persona=".$this->id;
+				$this->query->consulta($request);
+				$this->apellidos=$Apellidos;
+			}
 
-				$request="SELECT Nom_Area FROM empleados  JOIN area ON empleados.Id_Area=area.Id_Area WHERE Id_Empleado=".$this->id;
-				$result=$this->query->consulta($request);
-				$datos=$result->fetch_assoc();
-				return $datos["Nom_Area"];
+			public function get_dni()
+			{
+				return $this->dni;
+			}
+
+			public function cambiar_dni($Dni)
+			{
+				$request="UPDATE `personas` SET `Dni`='".$Dni."' WHERE Id_Persona=".$this->id;
+				$this->query->consulta($request);
+				$this->dni=$Dni;
 			}			
 
-			public function get_activo()
+			public function get_nombre_empresa()
 			{
-				return $this->activo;
-			}
+				return $this->nombre_empresa;
+			}		
 
-			public function get_correo()
+			public function cambiar_empresa($Empresa)
 			{
-				return $this->correo;
-			}
+				$request="UPDATE `personas` SET `Nombre_Empresa`='".$Empresa."' WHERE Id_Persona=".$this->id;
+				$this->query->consulta($request);
+				$this->nombre_empresa=$Empresa;
+			}					
+			
 
-			public function get_password()
-			{
-				return $this->password;
-			}
 			
 		}
-
-
+	
  ?>
 
-<?php 
-	/*
-	$persona= new Empleado(8);
-	echo $persona->get_id()."</br>";
+ <?php 
+ 	/*
+ 	$result=$persona=new Persona(7);
+
+ 	echo $persona->get_id()."</br>";
  	echo $persona->get_nombres()."</br>";
  	echo $persona->get_apellidos()."</br>";
  	echo $persona->get_dni()."</br>";
  	echo $persona->get_nombre_empresa()."</br>";
- 	echo $persona->get_id_cargo()."</br>";
- 	echo $persona->get_nombre_cargo()."</br>";
- 	echo $persona->get_id_area()."</br>";
- 	echo $persona->get_nombre_area()."</br>";
- 	echo $persona->get_activo()."</br>";
- 	echo $persona->get_correo()."</br>";
- 	echo $persona->get_password()."</br>";
-	*/
- ?>
+  	$persona->cambiar_nombres("Panfilo");
+  	$persona->cambiar_apellidos("Smith");
+  	$persona->cambiar_dni(66666666);
+  	$persona->cambiar_empresa("ACME");
+  	echo $persona->get_nombres()."</br>";
+  	echo $persona->get_apellidos()."</br>";
+  	echo $persona->get_dni()."</br>";
+  	echo $persona->get_nombre_empresa()."</br>";
+  	*/
+  ?>
