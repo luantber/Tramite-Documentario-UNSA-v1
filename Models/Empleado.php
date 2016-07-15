f0b01ba1e102bfd312bb4eb0370fb35a8ef3bace
texsmv <texs.mv@gmail.com>
2016-07-15 08:50:02 -0500

    Update Empleado.php

--- Models/Empleado.php
+++ Models/Empleado.php
@@ -4,23 +4,89 @@
 		*/
 		
 		use Models\Query as Query;
-
-		class Empleado
+		include_once "Include.php";
+		class Empleado extends Persona
 		{
-			var $Query;
+			
+			var $id_cargo;
+			var $id_area;
+			var $activo;
+			var $correo;
+			var $password;
 
-			var $nombre 
-			public function  __construct(){
-				$Query = = new Query();
+			public function  __construct($Id_Empleado){
+				$this->query =  new Query();
+				$request_empleado="SELECT `Id_Empleado`, `Id_Cargo`, `Id_Area`, `Activo`, `Correo`, `Dni_Empleado`, `Password` FROM `empleados` WHERE Id_Empleado=".$Id_Empleado;
+				$result_empleado=$this->query->consulta($request_empleado);
+				if($result_empleado->num_rows != 0){
+					$datos_empleado = $result_empleado->fetch_assoc();
+					$this->id=$datos_empleado["Id_Empleado"];
+					$this->id_cargo=$datos_empleado["Id_Cargo"];
+					$this->id_area=$datos_empleado["Id_Area"];
+					$this->activo=$datos_empleado["Activo"];
+					$this->correo=$datos_empleado["Correo"];
+					$this->id_cargo=$datos_empleado["Id_Cargo"];
+					$this->dni=$datos_empleado["Dni_Empleado"];
+					$this->password=$datos_empleado["Password"];
+				
+					$request_persona="SELECT `Id_Persona`, `Dni`, `Nombres`, `Apellidos`, `Nombre_Empresa` FROM `personas` WHERE Id_Persona=".$Id_Empleado;
+					$result2=$this->query->consulta($request_persona);
+				    $datos_persona = $result2->fetch_assoc();				    			
+				    $this->nombres=$datos_persona["Nombres"];
+				    $this->apellidos=$datos_persona["Apellidos"];
+				    $this->nombre_empresa=$datos_persona["Nombre_Empresa"];
+					return true;
+				}
+				else{
+					return false;
+				}
+
+					
+
 			}
 			
-			public function ver()
+			public function get_id_cargo()
 			{
-				echo "Funca .... ";
+				return $this->id_cargo;
 			}
 
+			public function get_id_area()
+			{
+				return $this->id_area;
+			}
+
+			public function get_activo()
+			{
+				return $this->activo;
+			}
+
+			public function get_correo()
+			{
+				return $this->correo;
+			}
+
+			public function get_password()
+			{
+				return $this->password;
+			}
 			
 		}
 
 
+ ?>
+
+<?php 
+	/*
+	$persona= new Empleado(8);
+	echo $persona->get_id()."</br>";
+ 	echo $persona->get_nombres()."</br>";
+ 	echo $persona->get_apellidos()."</br>";
+ 	echo $persona->get_dni()."</br>";
+ 	echo $persona->get_nombre_empresa()."</br>";
+ 	echo $persona->get_id_cargo()."</br>";
+ 	echo $persona->get_id_area()."</br>";
+ 	echo $persona->get_activo()."</br>";
+ 	echo $persona->get_correo()."</br>";
+ 	echo $persona->get_password()."</br>";
+	*/
  ?>
