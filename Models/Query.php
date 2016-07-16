14b7fc0da7d873042ea22bf2f9a132dcebcf9c77
texsmv <texs.mv@gmail.com>
2016-07-16 17:06:57 -0500

    Update x5

--- Models/Query.php
+++ Models/Query.php
@@ -17,6 +17,13 @@
         $this->username=$GLOBALS['user_bd']; 
         $this->password=$GLOBALS['pass_bd']; 
         // Check connection
+        //$this->servername="localhost";
+        //$this->dbname="database4"; 
+        //$this->username="root"; 
+        //$this->password=""; 
+
+        
+        
         $this->connection = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
         if ($this->connection->connect_error) {
             die("Connection failed: " . $this->connection->connect_error);
