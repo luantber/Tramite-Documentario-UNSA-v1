c3857ee97bdc589180b6c12e289369b7c7c67161
margarcuae <margar.tcuae@gmail.com>
2016-07-21 15:02:59 -0500

    errores 2

--- Views/tramites/barraTramites.php
+++ Views/tramites/barraTramites.php
@@ -1,8 +1,8 @@
-<nav role="navigation" class="navbar navbar-inverse">
+<nav class="navbar navbar-default">
 	<ul class="nav nav-tabs">
-		<li class="active"><a href="<?php echo URLM ?>/tramite/todos">Todos</a></li>
-		<li ><a href="<?php echo URLM ?>/tramite/proceso">En Proceso</a></li>
-		<li ><a href="<?php echo URLM ?>/tramite/finalizados">Finalizados</a></li>
-		<li ><a href="<?php echo URLM ?>/tramite/rechazados">Rechazados</a></li>
+		<li class="active"><a href="<?php echo URLM ?>/tramite">Todos</a></li>
+		<li ><a href="#">En Proceso</a></li>
+		<li ><a href="#">Finalizados</a></li>
+		<li ><a href="#">Rechazados</a></li>
 	</ul>
 </nav>
