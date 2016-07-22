b3f04a208a66325ab4f9dd69cf954d30ed5a4feb
margarcuae <margar.tcuae@gmail.com>
2016-07-22 12:52:56 -0500

    alejandro

--- Views/tramites/asignar.php
+++ Views/tramites/asignar.php
@@ -1,22 +1,13 @@
 <div class="container"> 
 
 <form id="nyform" action="<?php echo URLM."tramites/mover" ?>" method="POST">
-	<input id="idtramite" type="hidden" name="idtramite" value="">
-	<input type="number" name="destino">
-	<input type="submit">
-</form>
-<script >
-	document.getElementById('idtramite').value = data.id;
-</script>
-<p id="notipo" ></p>
 
 <h2>Delegacion de Trámite</h2>
-<form method="post" action="#">
 <div>
 	<div class="form-group">
 		<label for="tipo" class="col-sm-2 control-label" >Enviar a : </label>
 		<div class="col-sm-10">
-			<select name="tipo" class="form-control" id="tipo" >
+			<select name="destino" class="form-control" id="tipo" >
 				<option value="" >Seleccionar</option>
 				<script type="text/javascript">
               		//var opciones=["logistica","secretaria","Dirección Academica","Sistemas"];
@@ -31,5 +22,12 @@
 		</div>
 	</div>
 </div>
+	<input id="idtramite" type="hidden" name="idtramite" value="">
+		<input type="submit">
+
+
 </form>
 </div>
+<script >
+	document.getElementById('idtramite').value = data.id;
+</script>
