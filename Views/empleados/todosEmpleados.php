020ca6eb718e85d8c47a0bca19fd41ae25615f6d
margarcuae <margar.tcuae@gmail.com>
2016-07-25 20:13:35 -0500

    ++

--- Views/empleados/todosEmpleados.php
+++ Views/empleados/todosEmpleados.php
@@ -6,17 +6,20 @@ document.write(title);
   <table class="table table-striped">
   <thead>
       <tr>
-        <th>IDEmpledo</th>
+        <th>ID</th>
         <th>DNI</th>
         <th>Nombre</th>
         <th>Apellido</th>
+        <th>Área</th>
+        <th>Cargo</th>
+        <th>Estado</th>
+        <th>E-mail</th>
         <!--<th><li><a href="<?php echo URLM ?>empleados/crear">Registar</a></li></th>!-->
         <th><span class="glyphicon glyphicon-user"></span> </th>
-        <th><span class="glyphicon glyphicon-pencil"></span> </th>
         <th><span class="glyphicon glyphicon-trash"></span> </th>
       </tr>
     </thead>
-  
+
     <tbody>      
       <script type="text/javascript">
         var nuevo,tamanio;
@@ -28,11 +31,24 @@ document.write(title);
           document.write("<td>"+nuevo[1]+"</td>");
           document.write("<td>"+nuevo[2]+"</td>");
           document.write("<td>"+nuevo[3]+"</td>");
+          document.write("<td>"+nuevo[6]+"</td>");
+          document.write("<td>"+nuevo[8]+"</td>");
+          document.write("<td>"+nuevo[9]+"</td>");
+          document.write("<td>"+nuevo[10]+"</td>");
           document.write("<td><a href='<?php echo URLM ?>personas/Ver/"+nuevo[0]+"'><span class='glyphicon glyphicon-user'></span></a>"+"</td>");
-          document.write("<td><a href='<?php echo URLM ?>empleados/crear'><span class='glyphicon glyphicon-pencil'></span></a>"+"</td>");
           document.write("<td><a href='<?php echo URLM ?>personas/eliminar/"+ nuevo[0] +"'><span class='glyphicon glyphicon-trash'></span></a>"+"</td></tr>");
         }
       </script>
+
+    <div class="container">
+      <ul class="pagination">
+        <li><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+      </ul>
+    </div>
     </tbody>
   </table>
 </div>
--- Views/personas/todas.php
+++ Views/personas/todas.php
@@ -14,7 +14,6 @@ document.write(title);
         <!--<th><li><a href="<?php echo URLM ?>empleados/crear">Registar</a></li></th>!-->
         <th><span class="glyphicon glyphicon-user"></span> </th>
         <th><span class="glyphicon glyphicon-pencil"></span> </th>
-        <th><span class="glyphicon glyphicon-trash"></span> </th>
       </tr>
     </thead>
   
@@ -30,8 +29,7 @@ document.write(title);
           document.write("<td>"+nuevo[2]+"</td>");
           document.write("<td>"+nuevo[3]+"</td>");
           document.write("<td><a href='<?php echo URLM ?>personas/Ver/"+nuevo[0]+"'><span class='glyphicon glyphicon-user'></span></a>"+"</td>");
-          document.write("<td><a href='<?php echo URLM ?>empleados/crear'><span class='glyphicon glyphicon-pencil'></span></a>"+"</td>");
-          document.write("<td><a href='<?php echo URLM ?>personas/eliminar/"+ nuevo[0] +"'><span class='glyphicon glyphicon-trash'></span></a>"+"</td></tr>");
+          document.write("<td><a href='<?php echo URLM ?>empleados/crear'><span class='glyphicon glyphicon-pencil'></span></a>"+"</td></tr>");
         }
       </script>
     </tbody>
