cc8f4593b8ded94578999f93b4065d7352950a12
margarcuae <margar.tcuae@gmail.com>
2016-07-27 12:09:48 -0500

    more moer

--- Views/index/index.php
+++ Views/index/index.php
@@ -1,7 +1,11 @@
 
 <div id="Inicio" align="center">
 
-	<p>Hola , este es el index </p>
+	<img src="<?php echo URLV ?>imagenes/tramite.png" class="img-rounded" alt="Cinque Terre" height="380" >
+
+	<p ><i>Ingrese para comenzar</i></p>
+	<p><i>Si desea buscar un trámite no es necesario ingresar</i></p>
+	<p><b>Mas información en TUTORIAL</b></p>
 	
 
 </div>
