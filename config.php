42693bc9de1e603d5c38ee6b3f198c285c8954c0
luisbch9 <luisbch9@gmail.com>
2016-08-01 08:43:59 -0500

    conf

--- config.php
+++ config.php
@@ -7,18 +7,20 @@
 	$nombre_carpeta = "tramitedocumentariocs";
 
 
-//	define('URLV', 'http://192.168.43.213/'.$nombre_carpeta.'/Views/');
-//	define('URLM', 'http://192.168.43.213/'.$nombre_carpeta.'/');
+	$ip = "localhost";
 
-	define('URLV', 'http://localhost/'.$nombre_carpeta.'/Views/');
-	define('URLM', 'http://localhost/'.$nombre_carpeta.'/');
+	define('URLV', 'http://'.$ip.'/'.$nombre_carpeta.'/Views/');
+	define('URLM', 'http://'.$ip.'/'.$nombre_carpeta.'/');
+
+	//define('URLV', 'http://localhost/'.$nombre_carpeta.'/Views/');
+	//define('URLM', 'http://localhost/'.$nombre_carpeta.'/');
 
 	//Datos Futuros para conexion con bases de datos
 	global $server,$base_datos,$user_bd, $pass_bd;
 
 	// Listo para red local
-/*
-	$server = "192.168.43.213";
+	
+	$server = $ip;
 	$base_datos = "tramite";
 	$user_bd = "user";
 	$pass_bd = "12345";
